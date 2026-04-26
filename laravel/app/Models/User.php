6e350fcf9a233df7b7cf4d0cde7dec159eb2bce8
unknown <natthawut.niny@gmail.com>
2026-04-26 18:37:57 +0700

    feat: migrate user model and create user api endpoint

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,20 +2,63 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * Set to null to disable updated_at, as the legacy table only has created_at
+     */
+    const UPDATED_AT = null;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'steamid',
+        'username',
+        'email',
+        'google_id',
+        'password',
+        'generated_password',
+        'personaname',
+        'avatar',
+        'profileurl',
+        'role',
+        'rank',
+        'position',
+        'status',
+        'join_date',
+        'resume_data',
+        'parent_id',
+        'coc_sort_order',
+        'coc_x',
+        'coc_y',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'generated_password',
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -25,7 +68,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'join_date' => 'datetime',
             'password' => 'hashed',
         ];
     }
