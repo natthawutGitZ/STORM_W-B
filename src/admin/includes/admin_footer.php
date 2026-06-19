40e311eb62f7e04e39670015420ae96a64966c01
unknown <natthawut.niny@gmail.com>
2026-06-19 18:26:12 +0700

    fix: remove duplicate sidebar/topbar JS scripts from admin_footer.php

--- src/admin/includes/admin_footer.php
+++ src/admin/includes/admin_footer.php
@@ -1,7 +1,5 @@
             </div><!-- .storm-admin-page-body -->
         </main><!-- .storm-admin-content -->
     </div><!-- .storm-admin-layout -->
-    <script src="/assets/js/admin_sidebar.js"></script>
-    <script src="/assets/js/admin_topbar.js"></script>
 </body>
 </html>
